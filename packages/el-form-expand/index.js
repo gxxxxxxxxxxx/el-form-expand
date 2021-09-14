191caf09b23629966123b1c6ebd29bcb02546b66
ganxiaoxing <ganxiaoxing@junrunrenli.com>
2021-09-14 16:38:24 +0800

    feat: :tada: init example

---
+++ packages/el-form-expand/index.js
@@ -0,0 +1,141 @@
+export default {
+  props: {
+    btns: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      column: 4,
+      labelWidth: '25%',
+      isExpand: false,
+      uniKey: ''
+    }
+  },
+  created() {
+    this.uniKey = String(new Date().getTime())
+    window.addEventListener('resize', this.debounce(this.handleChangeColumns, 200))
+  },
+  destroyed() {
+    window.removeEventListener('resize', this.debounce(this.handleChangeColumns, 200))
+  },
+  methods: {
+    debounce(fn, time) {
+      let that = null
+      return () => {
+        clearTimeout(that)
+        that = setTimeout(() => {
+          fn()
+        }, time)
+      }
+    },
+    handleChangeColumns() {
+      console.log(111)
+      if (document.body.offsetWidth < 1220) {
+        this.column = 2
+        this.labelWidth = '50%'
+      } else if (document.body.offsetWidth < 1550) {
+        this.column = 3
+        this.labelWidth = '33%'
+      } else {
+        this.column = 4
+        this.labelWidth = '25%'
+      }
+    },
+    getRef() {
+      return this.$refs[this.uniKey]
+    }
+  },
+  render(h) {
+    const defaultSlots = this.$slots.default.map(item => <div style={{ width: this.labelWidth, transition: 'all 0.2s' }}>{item}</div>)
+
+    const expandIconStyle = {
+      marginLeft: '5px',
+      transition: 'all 0.3s ease 0s',
+      transform: this.isExpand ? 'rotate(0.5turn)' : 'rotate(0turn)'
+    }
+
+    const systemBtn = (
+      <div
+        style={{
+          fontSize: '14px',
+          cursor: 'pointer',
+          userSelect: 'none',
+          color: '#1890ff'
+        }}
+        onClick={() => {
+          this.isExpand = !this.isExpand
+          this.$emit('expandChange', this.isExpand)
+        }}
+      >
+        {' '}
+        {this.isExpand ? '收起' : '展开'}
+        <i style={expandIconStyle} class="el-icon-arrow-down"></i>
+      </div>
+    )
+
+    const renderBtnList = this.btns.slice(0).map(item =>
+      item instanceof h('span', '').constructor ? (
+        item
+      ) : typeof item.render === 'function' ? (
+        item.render()
+      ) : (
+        <el-button type={item.type || 'primary'} icon={`el-icon-${item.icon}`} onClick={item.click}>
+          {item.name}
+        </el-button>
+      )
+    )
+
+    const renderCustomBtns = (h, btns) => {
+      const btnsStyle = {
+        display: 'flex',
+        flexDirection: 'row-reverse'
+      }
+
+      const btnStyle = {
+        marginLeft: '10px'
+      }
+      return (
+        <div style={{ flex: 1 }}>
+          <el-form-item label-width={'0'}>
+            <div style={btnsStyle}>
+              {btns.map(btn => (
+                <div style={btnStyle}>{btn}</div>
+              ))}
+            </div>
+          </el-form-item>
+        </div>
+      )
+    }
+
+    renderBtnList.reverse()
+    if (defaultSlots.length - this.column >= 0) {
+      renderBtnList.unshift(systemBtn)
+    }
+    defaultSlots.push(renderCustomBtns(h, renderBtnList))
+    if (!this.isExpand) defaultSlots.slice(this.column - 1, defaultSlots.length - 1).forEach(item => (item.data.style = { ...item.data.style, display: 'none' }))
+
+    return h(
+      'el-form',
+      {
+        props: {
+          'label-width': '120px',
+          ...this.$attrs,
+
+          inline: false
+        },
+        ref: this.uniKey
+      },
+      [
+        h(
+          'div',
+          {
+            style: { display: 'flex', flexWrap: 'wrap' }
+          },
+          defaultSlots
+        )
+      ]
+    )
+  }
+}
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="search">
+      <ElFormPro ref="form" :rules="ruleForm" :model="form">
+        <el-form-item label="活动名称" prop="name">
+          <el-input v-model="form.name" />
+        </el-form-item>
+
+        <el-form-item label="活动名称2" prop="name">
+          <el-input v-model="form.name" />
+        </el-form-item>
+
+        <el-form-item label="活动名称3" prop="name">
+          <el-input v-model="form.name" />
+        </el-form-item>
+
+        <el-form-item label="活动名称4" prop="name">
+          <el-input v-model="form.name" />
+        </el-form-item>
+
+        <el-form-item label="活动名称5" prop="name">
+          <el-input v-model="form.name" />
+        </el-form-item>
+
+        <el-form-item label="活动名称6" prop="name">
+          <el-input v-model="form.name" />
+        </el-form-item>
+      </ElFormPro>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ElFormPro from '../packages/el-form-expand/index'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ElFormPro
+  },
+  data() {
+    return {
+      form: {},
+      ruleForm: {
+        name: [
+          { required: true, message: '请输入活动名称', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.$refs.form.getRef().validate()
+    }, 1000)
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.search {
+  margin: 0 auto;
+  width: 90%;
+  padding: 40px 24px;
+  border: 1px solid #ebedf1;
 }
 </style>
